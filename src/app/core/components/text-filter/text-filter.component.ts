b7b6332e4fdb572ca0c36c3d21bad90f0402bf68
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 11:48:55 +0000

    implement text filter

--- src/app/core/components/text-filter/text-filter.component.ts
+++ src/app/core/components/text-filter/text-filter.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-text-filter',
+  selector: 'text-filter',
   templateUrl: './text-filter.component.html',
-  styleUrls: ['./text-filter.component.scss']
+  styleUrls: ['./text-filter.component.scss'],
+  host: { class: 'c-textFilter' }
 })
 export class TextFilterComponent implements OnInit {
+  @Output() public filterChanged = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onfilterChanged(value: string) {
+    this.filterChanged.emit(value);
   }
 
 }

cec360157685e493dba03ea492bfb90f09c03b62
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 13:01:17 +0000

    implement date navigator component improve styling

--- src/app/core/components/date-navigator/date-navigator.component.ts
+++ src/app/core/components/date-navigator/date-navigator.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
 
 @Component({
-  selector: 'app-date-navigator',
+  selector: 'date-navigator',
   templateUrl: './date-navigator.component.html',
-  styleUrls: ['./date-navigator.component.scss']
+  styleUrls: ['./date-navigator.component.scss'],
+  host: { class: 'c-dateNavigator' }
 })
 export class DateNavigatorComponent implements OnInit {
+  currentDate: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.currentDate = moment(new Date()).format('dddd DD MMMM');
   }
-
 }

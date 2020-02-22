993db39c4cba15f5654f2b9c337a32d3e7555e96
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 17:53:44 +0000

    improve validations

--- src/app/authentication/pages/login/login.page.ts
+++ src/app/authentication/pages/login/login.page.ts
@@ -27,7 +27,8 @@ export class LoginPage implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      postCode: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(8)]
     });
 
     const returnUrlKey = 'returnUrl';

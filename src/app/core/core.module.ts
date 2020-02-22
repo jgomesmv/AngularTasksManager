a7cb26e2c1de53f1ab26df65d8ecd152057276d8
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 11:48:03 +0000

    implement filter pipe

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TextFilterComponent } from './components/text-filter/text-filter.component';
 import { DropdownFilterComponent } from './components/dropdown-filter/dropdown-filter.component';
 import { DateNavigatorComponent } from './components/date-navigator/date-navigator.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [TextFilterComponent, DropdownFilterComponent, DateNavigatorComponent],
+  declarations: [TextFilterComponent, DropdownFilterComponent, DateNavigatorComponent, FilterPipe],
   imports: [CommonModule],
-  exports: [TextFilterComponent, DropdownFilterComponent, DateNavigatorComponent]
+  exports: [TextFilterComponent, DropdownFilterComponent, DateNavigatorComponent, FilterPipe]
 })
 export class CoreModule {}
---
+++ src/app/core/pipes/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  public transform(value, keys: string, term: string) {
+    if (!term) {
+      return value;
+    }
+    const res = (value || []).filter(item =>
+      keys
+        .split(',')
+        .some(
+          key =>
+            item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])
+        )
+    );
+    return res.length ? res : [-1];
+  }
+}

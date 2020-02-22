d86dcafd24db52ce67d40f35d58edcba86fe1f2d
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 19:18:59 +0000

    bug fix group dropdown

--- src/app/core/models/select-list-item/select-list-item.ts
+++ src/app/core/models/select-list-item/select-list-item.ts
@@ -2,7 +2,6 @@ export class SelectListItem {
   disabled = false;
   selected = false;
   text: string;
-  subtext?: string;
   value: any;
 
   public constructor(selectListItem?: Partial<SelectListItem>) {

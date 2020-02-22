8eedc234701bf73d4720cceccc83590158368b59
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2020-02-22 18:13:23 +0000

    remove unit tests

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,11 +5,10 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TodayPage } from './pages/today/today.page';
 import { UserTaskGridComponent } from './pages/today/resources/users-grid/user-task-grid.component';
-import { StandbyTasksComponent } from './pages/today/resources/standby-tasks/standby-tasks.component';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [TodayPage, UserTaskGridComponent, StandbyTasksComponent],
+  declarations: [TodayPage, UserTaskGridComponent],
   imports: [CommonModule, TasksRoutingModule, DragDropModule, CoreModule],
   exports: [UserTaskGridComponent]
 })
